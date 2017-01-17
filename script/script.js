be837fb119a64e6e1f51a8d43a01a1e29390e08a
sasa-gligorov <gligorovsasa@gmail.com>
2017-01-17 01:54:52 +0100

    draw on mouseover

--- script/script.js
+++ script/script.js
@@ -1,11 +1,23 @@
-$(document).ready(function(){
-	var numOfRows = 4;
-	var numOfColumns = 4;
+function draw(){
+	$(".pixel").mouseenter(function(){
+		$(this).addClass("gray");
+	});
+}
+
+function createTable(size){
+
+	var numOfRows = size;
+	var numOfColumns = size;
 
 	for (var i = 1; i <= numOfRows; i++) {
 		$(".sketch-pixels").append('<tr class="row' + i + '"></tr>');
 		for (var j = 1; j <= numOfColumns; j++){
-				$(".row" + i).append('<td class="column' + j + '"></tr>');
+				$(".row" + i).append('<td class="column' + j + '"><div class="pixel"></div></tr>');
 			}
 		}
+}
+
+$(document).ready(function(){
+	createTable(32);
+	draw();
 });
